707caeddcb4a10759c99d9ca8d8efa43af57d716
Łukasz <kolak.lukass1@gmail.com>
2025-10-01 23:47:28 +0200

    implemented basic heart button

---
+++ components/gruby_page/heartButton.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { HeartIcon, HeartFilledIcon } from "@radix-ui/react-icons"
+
+export default function HeartButton({ onClick, disabled = false, children }) {
+  const [liked, setLiked] = useState(false);
+
+  return (
+    <button
+      onClick={() => {
+        setLiked(!liked);
+        onClick();
+      }}
+      className="relative transition-transform hover:scale-130 content-center align-middle"
+      disabled={disabled}
+    >
+      {liked ? (
+        <HeartFilledIcon className="w-8 h-8 text-red-500" />
+      ) : (
+        <HeartIcon className="w-8 h-8 text-gray-500" />
+      )}
+
+      {liked && (
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+          <div className="border-2 border-red-500 w-40 h-40 flex items-center justify-center">
+            {children}
+          </div>
+        </div>
+      )}
+    </button>
+  );
+}
