c093eb592cce90b3dce6775dc5cfd43ea94f434e
Łukasz <kolak.lukass1@gmail.com>
2025-10-08 20:26:01 +0200

    Added check for INTERNAL_MESSAGE_ID

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+if (!process.env.INTERNAL_MESSAGE_ID) {
+  throw new Error("INTERNAL_MESSAGE_ID is not defined in .env file, if this is unintentional create it: INTERNAL_MESSAGE_ID=\"...\" ");
+}
 const nextConfig = {
   images: {
     remotePatterns: [
