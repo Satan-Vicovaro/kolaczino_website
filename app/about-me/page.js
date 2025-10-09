90c76f52cd6921ab9a863f87b4580ded3d73a0ab
Łukasz <kolak.lukass1@gmail.com>
2025-10-09 19:23:11 +0200

    removed unnecessary files

--- app/about-me/page.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function AboutMe() {
-  return (
-    <div>
-      There is nothin interesing about me lol
-    </div>
-  )
-}
--- app/homestuck-mirror/page.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function HomestuckMirror() {
-  return (
-    <div>
-      maybe someday
-    </div>
-  )
-}
